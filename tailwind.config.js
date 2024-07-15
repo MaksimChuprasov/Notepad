a1474daa5f386fe462f719fa24124292824ea17e
Maksim Chuprasov <maxchuprasov07@gmail.com>
2024-07-15 16:09:48 +0300

    start project, connect tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./app/index.{js,jsx,ts,tsx}","./app/layout.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
