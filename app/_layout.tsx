d889455e0e714e47ca44eb5cbfef001a8d18ff90
Maksim Chuprasov <maxchuprasov07@gmail.com>
2025-01-20 14:09:22 +0300

    updated project for newe version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import CustomTabBar from '../components/CustomTabBar';
-import HomeView from '../views/HomeView';
-import SocialView from '../views/SocialView';
-import ShopView from '../views/ShopView';
-import NoteView from '../views/NoteView';
-import { NoteProvider } from './NoteContext';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CustomTabBar from "../components/CustomTabBar";
+import HomeView from "../views/HomeView";
+import SocialView from "../views/SocialView";
+import ShopView from "../views/ShopView";
+import NoteView from "../views/NoteView";
+import { NoteProvider } from "./NoteContext";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Tab = createBottomTabNavigator();
 
@@ -39,10 +40,12 @@ const MainTabNavigator = () => {
 
 export default function RootLayout() {
   return (
-    <NavigationContainer independent>
-      <NoteProvider>
-        <MainTabNavigator />
-      </NoteProvider>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer independent>
+        <NoteProvider>
+          <MainTabNavigator />
+        </NoteProvider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
--- views/NoteView.jsx
+++ views/NoteView.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, SafeAreaView, BackHandler } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 import * as DocumentPicker from 'expo-document-picker';
 import * as FileSystem from 'expo-file-system';
 import NoteContext from '../app/NoteContext';
 import SaveModal from '../components/SaveModal'
+import { PanGestureHandler } from 'react-native-gesture-handler';
+
 
 const NoteView = ({ navigation, route }) => {
     const { addNote, updateNote } = useContext(NoteContext);
@@ -20,30 +22,36 @@ const NoteView = ({ navigation, route }) => {
     const [isNavigating, setIsNavigating] = useState(false);
     const [editingFileIndex, setEditingFileIndex] = useState(-1);
     const [saveButtonLabel, setSaveButtonLabel] = useState('Save File');
+    const [Gptimage, setGptImage] = useState(require('../images/Chat_Gpt.png'));
+
+    const toggleImage = () => {
+        setGptImage((prevImage) =>
+            prevImage === require('../images/Chat_Gpt.png')
+                ? require('../images/Chat_Gpt_On.png')
+                : require('../images/Chat_Gpt.png')
+        );
+    };
 
     const isFocused = useIsFocused();
 
     useEffect(() => {
-        if (noteToEdit) {
-            setTitle(noteToEdit.title || '');
-            setText(noteToEdit.text || '');
-            setFiles(noteToEdit.files || []);
-        } else {
-            setTitle('');
-            setText('');
-            setFiles([]);
+        if (initialNoteToEdit) {
+            setTitle(initialNoteToEdit.title || '');
+            setText(initialNoteToEdit.text || '');
+            setFiles(initialNoteToEdit.files || []);
+            setGptImage(require('../images/Chat_Gpt.png'))
         }
-    }, [noteToEdit]);
+    }, [initialNoteToEdit]);
 
     const saveNote = () => {
         const updatedNote = {
-            id: noteToEdit ? noteToEdit.id : Date.now().toString(),
+            id: initialNoteToEdit ? initialNoteToEdit.id : Date.now().toString(),
             title,
             text,
             files
         };
 
-        if (noteToEdit) {
+        if (initialNoteToEdit) {
             updateNote(updatedNote);
         } else {
             addNote(updatedNote);
@@ -67,21 +75,6 @@ const NoteView = ({ navigation, route }) => {
 
         return unsubscribe;
     }, [navigation, text, title, files, initialNoteToEdit]);
-
-    useEffect(() => {
-        const unsubscribe = navigation.addListener('beforeRemove', (e) => {
-            const hasChanges = text !== initialNoteToEdit?.text ||
-                title !== initialNoteToEdit?.title ||
-                files.length !== (initialNoteToEdit?.files?.length || 0);
-
-            if (!isNavigating && hasChanges) {
-                e.preventDefault();
-                setShowSaveModal(true);
-            }
-        });
-
-        return unsubscribe;
-    }, [navigation, text, title, files, initialNoteToEdit, isNavigating]);
 
     const selectFiles = async () => {
         try {
@@ -134,6 +127,7 @@ const NoteView = ({ navigation, route }) => {
         setFileUri(fileUri);
         setSaveButtonLabel('Save Changes');
         setIsEditing(true);
+        setGptImage(Gptimage);
         try {
             const fileContent = await FileSystem.readAsStringAsync(fileUri, {
                 encoding: FileSystem.EncodingType.UTF8,
@@ -144,14 +138,18 @@ const NoteView = ({ navigation, route }) => {
         }
     };
 
+    const deleteFile = (index) => {
+        setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+    };
+
     const handleExitWithoutSaving = () => {
         setShowSaveModal(false);
+        setGptImage(require('../images/Chat_Gpt.png'))
         setIsNavigating(true);
         setTimeout(() => {
             navigation.goBack();
         }, 100);
     };
-
     const handleOutsidePress = () => {
         if (showSaveModal) {
             setShowSaveModal(false);
@@ -162,86 +160,156 @@ const NoteView = ({ navigation, route }) => {
         if (text !== noteToEdit?.text ||
             title !== noteToEdit?.title ||
             files.length !== (noteToEdit?.files?.length || 0)) {
-                setShowSaveModal(true);
+            setShowSaveModal(true);
         } else {
             navigation.goBack();
         }
     };
-    
+
+    useEffect(() => {
+        const backAction = () => {
+            handleBackPress();
+            return true;
+        };
+
+        const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+        return () => backHandler.remove();
+    }, [text, title, files, noteToEdit]);
+
+    const handleSwipe = (event) => {
+        if (event.nativeEvent.translationX < -100) {
+            handleBackPress();
+        }
+    }
+
     return (
         <SafeAreaView className=" h-full pt-9 bg-white">
-            <View className="flex-row items-center">
-                <TouchableOpacity className="mx-2" onPress={handleBackPress}>
-                    <Image
-                        source={require('../images/back.png')}
-                        className="w-8 h-8 mt-4"
-                    />
-                </TouchableOpacity>
-                <TextInput
-                    className="p-2 mt-2 bg-white text-xl font-bold w-full"
-                    placeholder='New Note'
-                    onChangeText={(title) => setTitle(title)}
-                    value={title}
-                    multiline={true}
-                />
-            </View>
-            <TextInput
-                className="p-2 mt-2 mx-2 bg-white"
-                placeholder='Enter your note...'
-                onChangeText={(text) => setText(text)}
-                value={text}
-                multiline={true}
-            />
-            <View className="px-3">
-                {files.map((file, index) => (
-                    <View key={index}>
-                        <TouchableOpacity className="w-1/2 mb-1"
-                            onPress={() => openFile(file.uri, index)}
-                        >
-                            <Text className="text-blue-600">{file.name}</Text>
-                        </TouchableOpacity>
-                        {editingFileIndex === index && (
-                            <TextInput className="border p-2 rounded-lg"
-                                onChangeText={(content) => setEditedFileContent(content)}
-                                placeholder='Change your file...'
-                                value={editedFileContent}
-                                multiline={true}
+            <PanGestureHandler onGestureEvent={handleSwipe}>
+                <View className="flex-1">
+                    <View className="flex-row items-center">
+                        <TouchableOpacity className="mx-2" onPress={handleBackPress}>
+                            <Image
+                                source={require('../images/back.png')}
+                                className="w-8 h-8 mt-4"
                             />
-                        )}
+                        </TouchableOpacity>
+                        <TextInput
+                            className="p-2 mt-2 bg-white text-xl font-bold w-2/3"
+                            placeholder='New Note'
+                            onChangeText={(title) => setTitle(title)}
+                            value={title}
+                            multiline={true}
+                        />
+                        <TouchableOpacity className="mx-2" onPress={toggleImage}>
+                            <Image
+                                source={Gptimage}
+                                className="w-8 h-8 mt-4"
+                            />
+                        </TouchableOpacity>
                     </View>
-                ))}
-            </View>
-            <View className="flex-1 flex-row">
-                {editingFileIndex !== -1 && (
-                    <TouchableOpacity
-                        className="flex items-center p-2 mx-1 mt-1 rounded-xl h-12"
-                        onPress={saveEditedFile}
-                    >
-                        <Image
-                            source={require('../images/save-file.png')}
-                            className="w-8 h-8"
-                        />
-                    </TouchableOpacity>
-                )}
-                {editingFileIndex === -1 && (
-                    <TouchableOpacity
-                        className="flex items-center p-2 mx-1 mt-1 rounded-xl h-12"
-                        onPress={selectFiles}
-                    >
-                        <Image
-                            source={require('../images/add-file.png')}
-                            className="w-8 h-8"
-                        />
-                    </TouchableOpacity>
-                )}
-            </View>
+                    <TextInput
+                        className="p-2 mt-2 mx-2 bg-white"
+                        placeholder='Enter your note...'
+                        onChangeText={(text) => setText(text)}
+                        value={text}
+                        multiline={true}
+                    />
+                    <View className="px-3">
+                        {files.map((file, index) => (
+                            <View key={index}>
+                                <TouchableOpacity className="w-1/2 mb-1"
+                                    onPress={() => openFile(file.uri, index)}
+                                    onLongPress={() => deleteFile(index)}
+                                >
+                                    <Text className="text-blue-600">{file.name}</Text>
+                                </TouchableOpacity>
+                                {editingFileIndex === index && (
+                                    <TextInput className="border p-2 rounded-lg"
+                                        onChangeText={(content) => setEditedFileContent(content)}
+                                        placeholder='Change your file...'
+                                        value={editedFileContent}
+                                        multiline={true}
+                                    />
+                                )}
+                            </View>
+                        ))}
+                    </View>
 
-            <SaveModal
-                visible={showSaveModal}
-                onExit={handleExitWithoutSaving}
-                onSave={saveNote}
-                onClose={handleOutsidePress}
-            />
+                    <View className="bg-white py-2 px-6 absolute bottom-0 left-0 w-full">
+                        <View className="flex-row justify-between">
+                            {/* Первая кнопка */}
+                            <View className="bg-white items-center w-12">
+                                {editingFileIndex !== -1 && (
+                                    <TouchableOpacity
+                                        className="flex items-center rounded-xl"
+                                        onPress={saveEditedFile}
+                                    >
+                                        <Image
+                                            source={require('../images/save-file.png')}
+                                            className="w-8 h-8"
+                                        />
+                                        <Text className="text-[12px]">Save File</Text>
+                                    </TouchableOpacity>
+                                )}
+                                {editingFileIndex === -1 && (
+                                    <TouchableOpacity
+                                        className="flex items-center rounded-xl"
+                                        onPress={selectFiles}
+                                    >
+                                        <Image
+                                            source={require('../images/add-file.png')}
+                                            className="w-8 h-8"
+                                        />
+                                        <Text className="text-[12px]">Add File</Text>
+                                    </TouchableOpacity>
+                                )}
+                                
+                            </View>
+
+                            {/* Вторая кнопка */}
+                            <TouchableOpacity
+                                className="bg-white items-center"
+                            >
+                                <Image
+                                    source={require('../images/plus.png')}
+                                    className="w-8 h-8"
+                                />
+                                <Text className="text-[12px]">Add</Text>
+                            </TouchableOpacity>
+
+                            {/* Третья кнопка */}
+                            <TouchableOpacity
+                                className="bg-white items-center"
+                            >
+                                <Image
+                                    source={require('../images/txtFormate.png')}
+                                    className="w-8 h-8"
+                                />
+                                <Text className="text-[12px]">Formate text</Text>
+                            </TouchableOpacity>
+
+                            {/* Кнопка создания заметки */}
+                            <TouchableOpacity
+                                className="bg-white items-center"
+                            >
+                                <Image
+                                    source={require('../images/collaborator.png')}
+                                    className="w-8 h-8"
+                                />
+                                <Text className="text-[12px]">Collabolator</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    <SaveModal
+                        visible={showSaveModal}
+                        onExit={handleExitWithoutSaving}
+                        onSave={saveNote}
+                        onClose={handleOutsidePress}
+                    />
+                </View>
+            </PanGestureHandler>
         </SafeAreaView>
     );
 };
